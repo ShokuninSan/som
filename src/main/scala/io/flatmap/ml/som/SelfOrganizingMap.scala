acde64eec3f1f9375091294f2c541b009cbf4c88
Shokunin San <me@flatmap.io>
2016-11-27 01:02:29 +0100

    Changed types of data points.

--- src/main/scala/io/flatmap/ml/som/SelfOrganizingMap.scala
+++ src/main/scala/io/flatmap/ml/som/SelfOrganizingMap.scala
@@ -2,15 +2,18 @@ package io.flatmap.ml.som
 
 import breeze.linalg.DenseMatrix
 import org.apache.spark.rdd.RDD
+import org.apache.spark.mllib.linalg.Vector
 
 class SelfOrganizingMap(width: Int, height: Int, sigma: Double = 0.2, learningRate: Double = 0.1) extends Serializable {
 
-  val codebook: DenseMatrix[Double] = DenseMatrix.zeros[Double](height, width)
+  type Neuron = (Int, Int)
 
-  def initialize(data: RDD[Double]): Unit =
+  val codebook: DenseMatrix[Array[Double]] = DenseMatrix.zeros[Array[Double]](height, width)
+
+  def initialize(data: RDD[Vector]): Unit =
     for {
       index <- codebook.keysIterator.toArray
       sample <- data.takeSample(withReplacement = true, width * height)
-    } yield codebook(index._1, index._2) = sample
+    } yield codebook(index._1, index._2) = sample.toArray
 
 }
