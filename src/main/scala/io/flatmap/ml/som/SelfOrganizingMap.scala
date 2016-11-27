5993665a2c0d000c35fe6c80f482948e26d78005
Shokunin San <me@flatmap.io>
2016-11-27 10:52:49 +0100

    Added method for finding best matching unit (BMU).
    Implementation should be refactored and simplified for better readability.

--- src/main/scala/io/flatmap/ml/som/SelfOrganizingMap.scala
+++ src/main/scala/io/flatmap/ml/som/SelfOrganizingMap.scala
@@ -1,8 +1,8 @@
 package io.flatmap.ml.som
 
-import breeze.linalg.DenseMatrix
-import org.apache.spark.rdd.RDD
+import breeze.linalg.{DenseMatrix, DenseVector, norm}
 import org.apache.spark.mllib.linalg.Vector
+import org.apache.spark.rdd.RDD
 
 class SelfOrganizingMap(width: Int, height: Int, sigma: Double = 0.2, learningRate: Double = 0.1) extends Serializable {
 
@@ -15,5 +15,14 @@ class SelfOrganizingMap(width: Int, height: Int, sigma: Double = 0.2, learningRa
       index <- codebook.keysIterator.toArray
       sample <- data.takeSample(withReplacement = true, width * height)
     } yield codebook(index._1, index._2) = sample.toArray
+
+  def winner(point: Vector): Neuron = {
+    val activationMap = DenseMatrix.zeros[Double](height, width)
+    val subtracted = codebook.copy.mapValues(_.zip(point.toArray).map { case (a,b) => a-b })
+    subtracted.foreachPair { case ((x, y), v) =>
+        activationMap(x, y) = norm(DenseVector(v))
+    }
+    activationMap.argmin
+  }
 
 }
--- src/test/scala/io/flatmap/ml/som/SelfOrganizingMapSpec.scala
+++ src/test/scala/io/flatmap/ml/som/SelfOrganizingMapSpec.scala
@@ -1,6 +1,7 @@
 package io.flatmap.ml.som
 
 import breeze.linalg.DenseMatrix
+import org.apache.spark.mllib.linalg.DenseVector
 import org.apache.spark.mllib.random.RandomRDDs
 import org.scalatest._
 import util.TestSparkContext
@@ -17,6 +18,21 @@ class SelfOrganizingMapSpec extends FlatSpec with Matchers with BeforeAndAfterEa
     val som = new SelfOrganizingMap(6, 6)
     som.initialize(data)
     som.codebook should !== (DenseMatrix.fill[Array[Double]](6, 6)(Array.emptyDoubleArray))
+  }
+
+  "winner" should "return best matching unit (BMU)" in {
+    val som = new SelfOrganizingMap(6, 6)
+    som.codebook.keysIterator.foreach { case (x, y) => som.codebook(x, y) = Array(0.2, 0.2, 0.2) }
+    som.codebook(3, 3) = Array(0.3, 0.3, 0.3)
+    som.winner(new DenseVector(Array(2.0, 2.0, 2.0))) should equal ((3, 3))
+    som.winner(new DenseVector(Array(0.26, 0.26, 0.26))) should equal ((3, 3))
+  }
+
+  "winner" should "return first index in case of multiple best matching units (BMU)" in {
+    val som = new SelfOrganizingMap(6, 6)
+    som.codebook.keysIterator.foreach { case (x, y) => som.codebook(x, y) = Array(0.2, 0.2, 0.2) }
+    som.codebook(3, 3) = Array(0.3, 0.3, 0.3)
+    som.winner(new DenseVector(Array(0.25, 0.25, 0.25))) should equal ((0, 0))
   }
 
 }
