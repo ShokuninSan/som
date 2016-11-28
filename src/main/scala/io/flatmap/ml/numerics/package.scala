0d8035fbfa691faf1d75a6bb224718cdd32f3d6d
Shokunin San <me@flatmap.io>
2016-11-28 21:54:25 +0100

    Added context bounds for generic 'outer' function.

--- src/main/scala/io/flatmap/ml/numerics/package.scala
+++ src/main/scala/io/flatmap/ml/numerics/package.scala
@@ -1,11 +1,14 @@
 package io.flatmap.ml
 
 import breeze.linalg.{*, DenseMatrix, DenseVector}
+import breeze.math.Field
+
+import scala.reflect._
 
 package object numerics {
 
-  def outer[T](x: DenseVector[T], y: DenseVector[T]): DenseMatrix[T] = {
-    val g = DenseMatrix.zeros[Double](y.length, x.length)
+  def outer[T:ClassTag:Field](x: DenseVector[T], y: DenseVector[T]): DenseMatrix[T] = {
+    val g = DenseMatrix.zeros[T](y.length, x.length)
     g(::, *) := y
     g(*, ::) *:* x
   }
