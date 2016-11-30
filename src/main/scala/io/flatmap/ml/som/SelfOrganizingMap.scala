0de7145262395e88577eb92acb669b8d49e7810f
Shokunin San <me@flatmap.io>
2016-11-30 21:50:48 +0100

    Added train method and loosened RDD type parameters.

--- src/main/scala/io/flatmap/ml/som/SelfOrganizingMap.scala
+++ src/main/scala/io/flatmap/ml/som/SelfOrganizingMap.scala
@@ -27,7 +27,7 @@ class SelfOrganizingMap private (val codeBook: CodeBook, val sigma: Double, val 
   private val width = codeBook.cols
   private val height = codeBook.rows
 
-  def initialize(data: RDD[Vector]): Unit =
+  def initialize[T <: Vector](data: RDD[T]): Unit =
     for {
       (i, j) <- codeBook.keysIterator.toArray
       sample <- data.takeSample(withReplacement = true, width * height)
@@ -58,6 +58,39 @@ class SelfOrganizingMap private (val codeBook: CodeBook, val sigma: Double, val 
     val gaussianY = gaussianVector(height, winner._2)
     // return 2 dimensional gaussian dist. surface by creating the outer product
     outer(gaussianX, gaussianY)
+  }
+
+  def train[T <: Vector](data: RDD[T], iterations: Int, partitions: Int = 12)(implicit sparkSession: SparkSession): SelfOrganizingMap = {
+    var iteration = 0
+    var codeBook = this.codeBook.copy
+    for (iteration <- 0 until iterations) {
+      val sigma = decay(this.sigma, iteration, iterations)
+      val learningRate = decay(this.learningRate, iteration, iterations)
+      val broadcastedCodeBook = sparkSession.sparkContext.broadcast(codeBook)
+      val randomizedRDD = data.repartition(partitions)
+      print(s"iter: $iteration, sigma: $sigma, learningRate: $learningRate")
+      def trainPartition = (dataPartition: Iterator[Vector]) => {
+        val localCodebook = broadcastedCodeBook.value
+        dataPartition.foreach { dataPoint =>
+          val bestMatchingUnit = winner(dataPoint)
+          gaussian(bestMatchingUnit).foreachPair {
+            case ((i, j), h) =>
+              localCodebook(i, j) =
+                (DenseVector(localCodebook(i, j))
+                  + learningRate
+                  * h
+                  * (DenseVector(dataPoint.toArray)
+                    - DenseVector(localCodebook(i, j)))).toArray
+          }
+        }
+        Array(localCodebook).iterator
+      }
+      val resultCodeBook = randomizedRDD.mapPartitions(trainPartition)
+      val newCodebook = resultCodeBook.reduce(_ + _)
+      newCodebook.map(v => v.map(_ / partitions.toDouble))
+      codeBook = newCodebook
+    }
+    new SelfOrganizingMap(codeBook, sigma, learningRate)
   }
 
 }
