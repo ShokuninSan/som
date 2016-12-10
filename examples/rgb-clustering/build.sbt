b5505cec05e7cc8394ee48255fc29b9510d6236e
Shokunin San <me@flatmap.io>
2016-12-11 00:53:57 +0100

    Use provided Plot API for RGB example.

--- examples/rgb-clustering/build.sbt
+++ examples/rgb-clustering/build.sbt
@@ -8,12 +8,8 @@ scalaVersion := "2.11.7"
 
 val sparkVersion = "2.0.2"
 
-val breezeVersion = "0.11.2"
-
 libraryDependencies ++= Seq(
   "io.flatmap" %% "som" % "1.0.0-SNAPSHOT",
-  "org.scalanlp" % "breeze_2.11" % breezeVersion,
-  "org.scalanlp" % "breeze-viz_2.11" % breezeVersion,
   "org.apache.spark" %% "spark-core" % sparkVersion % "provided",
   "org.apache.spark" %% "spark-mllib" % sparkVersion % "provided"
 )
--- examples/rgb-clustering/src/main/scala/example/RGB.scala
+++ examples/rgb-clustering/src/main/scala/example/RGB.scala
@@ -2,26 +2,12 @@ package example
 
 import java.awt.Color
 
-import breeze.plot._
 import io.flatmap.ml.som.SelfOrganizingMap
-import io.flatmap.ml.som.SelfOrganizingMap.CodeBook
+import io.flatmap.ml.util.Plot
 import org.apache.spark.mllib.linalg.DenseVector
 import org.apache.spark.sql.SparkSession
 
-import scala.util.Try
-
 object RGB {
-
-  private def plot(title: String, cb: CodeBook, path: String) = Try {
-    val f = Figure(title)
-    val rgb: List[Double] => Double = {
-      case red :: green :: blue :: Nil =>
-        new Color((red*255.0).toInt, (green*255.0).toInt, (blue*255.0).toInt).getRGB.toDouble
-      case _ => Color.white.getRGB.toDouble
-    }
-    f.subplot(0) += image(cb.mapValues(x => rgb(x.toList)))
-    f.saveas(path)
-  }
 
   def main(args: Array[String]): Unit = {
     implicit val sparkSession =
@@ -37,7 +23,11 @@ object RGB {
       SelfOrganizingMap(24, 24, sigma = 0.5, learningRate = 0.3)
         .initialize(rgb)
         .train(rgb, 20)
-    plot(f"Trained SOM (error=${params.error}%1.4f)", som.codeBook, "trained_som.png")
+    Plot.som(f"Trained SOM (error=${params.error}%1.4f)", som.codeBook, "trained_som.png") {
+      case red :: green :: blue :: Nil =>
+        new Color((red*255.0).toInt, (green*255.0).toInt, (blue*255.0).toInt).getRGB.toDouble
+      case _ => Color.white.getRGB.toDouble
+    }
   }
 
 }
